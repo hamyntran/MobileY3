3152c5fcea222af04dd59bd4f337c796248c8a63
hamyntran <nguyentranhamy2002@gmail.com>
2022-10-16 23:32:37 +0700

    aset

---
+++ PortfolioMobileArcade/Assets/Scripts/Abstact1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abstact1 : TestAbstact
+{
+    public override void TestHealth()
+    {
+    }
+
+    private void Start()
+    {
+        TestHealth();
+    }
+}
---
+++ PortfolioMobileArcade/Assets/Scripts/TestAbstact.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TestAbstact
+{
+    public int health = 10;
+    private float _timer = 100f;
+
+    public abstract void TestHealth();
+    
+}
