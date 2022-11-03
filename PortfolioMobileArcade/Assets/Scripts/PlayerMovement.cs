f22e632016e32a6c14679ac8e112fa8bcd62773e
hamyntran <69077978+hamyntran@users.noreply.github.com>
2022-11-03 22:43:38 +0700

    movement

--- PortfolioMobileArcade/Assets/Scripts/PlayerMovement.cs
+++ PortfolioMobileArcade/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public  
+    [SerializeField] private int gap = 1;
+
+    [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private float moveSpeed = .5f;
+    [SerializeField] private float rotateSpeed = .5f;
+
+    private Rigidbody playerRG;
+
+    public Vector3 nextDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRG = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    
+    private void HandleInput() {
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) {
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+                //Move(new Vector3(-1, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+                //Move(new Vector3(1, 0, 0));
+        }
     }
 }
