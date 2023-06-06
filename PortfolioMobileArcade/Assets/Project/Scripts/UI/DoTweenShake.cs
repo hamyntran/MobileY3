16b593fa551578010d21510af06a80de90502457
hamyntran <69077978+hamyntran@users.noreply.github.com>
2023-06-06 22:42:44 +0700

    dotween

---
+++ PortfolioMobileArcade/Assets/Project/Scripts/UI/DoTweenShake.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoTweenShake : MonoBehaviour
+{
+   [SerializeField] private float _duration;
+   public void Shake()
+   {
+      //gameObject.transform.DOShakeScale( )
+   }
+}
